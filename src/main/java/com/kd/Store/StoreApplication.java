21626f63c9cacf8e2cc9fd9474102219a7eb7f72
Kalana_KD <kalanakd471@gmail.com>
2025-08-12 10:27:04 +0530

    Create Store Application

---
+++ src/main/java/com/kd/Store/StoreApplication.java
@@ -0,0 +1,20 @@
+package com.kd.Store;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+}
